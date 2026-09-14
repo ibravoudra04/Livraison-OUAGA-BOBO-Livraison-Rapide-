63ef8566abfa14f25daacd50c819861c77bb2bf1
Ibrahim <ibravoudra04@gmail.com>
2026-09-14 16:26:13 +0000

    fix: carte epuree CartoDB Positron

--- components/Map/MapComponent.tsx
+++ components/Map/MapComponent.tsx
@@ -123,9 +123,10 @@ export default function MapComponent({ livreurs = [], cityCenter = { lat: 12.371
         preferCanvas: true,
       }).setView([cityCenter.lat, cityCenter.lng], 14);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors'
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        subdomains: 'abcd',
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions" target="_blank" rel="noopener noreferrer">CARTO</a>'
       }).addTo(mapRef.current);
 
       markersRef.current = L.layerGroup().addTo(mapRef.current);
